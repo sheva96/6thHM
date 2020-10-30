e1a4ceadc441f392e496eca34fa518a02ac359de
Yevhen Shevchenko <sheva9620@gmail.com>
2020-10-30 15:28:01 +0200

    Initial Commit

---
+++ 6thHM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6thHM
+//
+//  Created by Евгений Шевченко on 30.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
